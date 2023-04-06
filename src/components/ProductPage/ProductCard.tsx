ee90e4dd6f1d8582d3179fe986826371e9c3ca95
checkiamsiam <issiam02415@gmail.com>
2023-04-07 01:06:25 +0600

    product order confirm modal

--- src/components/ProductPage/ProductCard.tsx
+++ src/components/ProductPage/ProductCard.tsx
@@ -1,8 +1,15 @@
 import { ProductCardProps } from "@/Types/Interface/ComponentProps/Product";
-import Image from "next/image";
+import Image from "next/image"; 
+import { useState } from "react";
 import happyFood from "@/assets/image/yellow minimalist healthy food banner.png";
-const ProductCard = ({ product }: ProductCardProps) => {
-  const { name, image, rating, price, description } = product;
+
+const ProductCard = ({ product } : ProductCardProps) => {
+  const { name, rating, price, description, image } = product;
+  const [showModal, setShowModal] = useState<Boolean>(false);
+
+  const handleOrderNow = () => {
+    setShowModal(true);
+  };
 
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
@@ -15,12 +22,14 @@ const ProductCard = ({ product }: ProductCardProps) => {
           <div className="flex items-center mb-2">
             <div className="flex items-center text-yellow-500">
               {[...Array(Math.round(rating))].map((_, index) => (
-                <svg key={index} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 14.795l-4.265 2.23a1 1 0 0 1-1.451-1.054l.814-4.739-3.451-3.364a1 1 0 0 1 .562-1.706l4.81-.7L9.397 1.38a1 1 0 0 1 1.206 0l2.24 1.178c.537.282.748.954.452 1.476l-3.256 3.992.814 4.738a1 1 0 0 1-1.452 1.054L10 14.795z"
-                    clipRule="evenodd"
-                  />
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  {/* SVG path for rating stars */}
                 </svg>
               ))}
             </div>
@@ -29,8 +38,70 @@ const ProductCard = ({ product }: ProductCardProps) => {
           <p className="text-gray-700 mb-4">${price}</p>
           <p className="text-gray-700">{description}</p>
         </div>
-        <button className="w-full py-3 text-white bg-indigo-500 hover:bg-indigo-600 font-semibold uppercase transition duration-200">Order Now</button>
+        <button
+          onClick={handleOrderNow}
+          className="w-full py-3 text-white bg-indigo-500 hover:bg-indigo-600 font-semibold uppercase transition duration-200"
+        >
+          Order Now
+        </button>
       </div>
+
+      {/* Modal for order confirmation */}
+      {showModal && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+              onClick={() => setShowModal(false)}
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+
+            <div
+              className="inline-block align-middle bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full sm:p-6"
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                  Confirm Order
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{name}</p>
+                  <p 
+                  className="mt-1 text-sm text-gray-500">{description}</p>
+                  <p className="mt-1 text-sm text-gray-500">${price}</p>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  onClick={() => setShowModal(false)}
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => {
+                    // Add logic for order confirmation here
+                    setShowModal(false);
+                  }}
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-500 text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-400 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ProductPage/ProductsSection.tsx
+++ src/components/ProductPage/ProductsSection.tsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
-import ProductCard from "./ProductCard"; // assuming a ProductCard component for displaying each product
 import { ProductSectionProps } from "@/Types/Interface/ComponentProps/Product";
+import { Product } from "@/Types/Interface/Product";
+import { useState } from "react";
+import ProductCard from "./ProductCard"; // assuming a ProductCard component for displaying each product
 
-const ProductsSection = ({ products } : ProductSectionProps) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 8; // number of products to display per page
+const ProductsSection = ({ products }: ProductSectionProps) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const productsPerPage: number = 8; // number of products to display per page
 
   // Calculate total number of pages based on products data and productsPerPage
-  const totalPages = Math.ceil(products.length / productsPerPage);
+  const totalPages: number = Math.ceil(products.length / productsPerPage);
 
   // Get products to display for the current page
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const productsToDisplay = products.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
+  const indexOfLastProduct: number = currentPage * productsPerPage;
+  const indexOfFirstProduct: number = indexOfLastProduct - productsPerPage;
+  const productsToDisplay: Product[] = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
   // Handle pagination - go to previous page
   const goToPreviousPage = () => {
@@ -32,34 +30,22 @@ const ProductsSection = ({ products } : ProductSectionProps) => {
       <h2 className="text-3xl text-center mb-6">Our Products</h2>
       <div className="flex flex-wrap justify-center items-center">
         {/* Render ProductCard component for each product */}
-        {productsToDisplay.map((product) => (
+        {productsToDisplay.map((product: Product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
       <div className="flex justify-center items-center gap-5 mt-6">
         {/* Render pagination buttons */}
-        <button
-          onClick={goToPreviousPage}
-          disabled={currentPage === 1}
-          className="bg-indigo-500 text-white font-bold py-2 px-4 rounded-md mr-2"
-        >
+        <button onClick={goToPreviousPage} disabled={currentPage === 1} className="bg-indigo-500 text-white font-bold py-2 px-4 rounded-md mr-2">
           Previous
         </button>
-        <span className="text-gray-600 text-xl font-semibold">
-          {currentPage}
-        </span>
-        <button
-          onClick={goToNextPage}
-          disabled={currentPage === totalPages}
-          className="bg-indigo-500 text-white font-bold py-2 px-4 rounded-md ml-2"
-        >
+        <span className="text-gray-600 text-xl font-semibold">{currentPage}</span>
+        <button onClick={goToNextPage} disabled={currentPage === totalPages} className="bg-indigo-500 text-white font-bold py-2 px-4 rounded-md ml-2">
           Next
         </button>
       </div>
     </section>
   );
 };
-
-
 
 export default ProductsSection;
